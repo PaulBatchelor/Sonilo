126cf794bdc879a0d8ca66bbf7bc28eebf0af51c
Paul Batchelor <thisispaulbatchelor@gmail.com>
2025-11-30 11:52:31 -0500

    docgen

---
+++ docgen.rs
@@ -0,0 +1,99 @@
+use std::io;
+use std::fs;
+use std::collections::{BTreeMap, HashMap, BTreeSet};
+use std::error::Error;
+
+#[derive(PartialEq)]
+enum ParserMode {
+    PRINT,
+    COMMENT,
+    MEMWRITE,
+    UPDATE,
+    LABEL,
+}
+
+enum Object {
+    Label(String),
+    Comment(String),
+    Code(String),
+}
+
+fn main() -> io::Result<()> {
+    let filename = "input.txt";
+    let data = fs::read(filename)?;
+    let mut mode;
+    let mut buf: Vec<_> = vec![];
+    let mut obj: Vec<Object> = vec![];
+
+    mode = ParserMode::PRINT;
+
+    for c in data {
+
+        if matches!(mode, ParserMode::UPDATE) {
+            if c == b'l' {
+                mode = ParserMode::LABEL;
+            } else if c == b'c' {
+                mode = ParserMode::COMMENT;
+            } else if c == b'm' {
+                mode = ParserMode::MEMWRITE;
+            } else if c == b'p' {
+                mode = ParserMode::PRINT;
+            }
+            buf.clear();
+            continue
+        }
+
+        if c == b'@' {
+            let str = match String::from_utf8(buf.clone()) {
+                Ok(s) => s,
+                Err(_) => panic!("oops could not decode"),
+            };
+            let str = str.trim().to_string().replace("_", "\\_").replace("#", "\\#");
+            match mode {
+                ParserMode::LABEL => {
+                    obj.push(Object::Label(str));
+                },
+                ParserMode::COMMENT => {
+                    obj.push(Object::Comment(str));
+                },
+                ParserMode::MEMWRITE => {
+                    obj.push(Object::Code(str));
+                }
+                _ => {},
+            }
+            mode = ParserMode::UPDATE;
+            continue;
+        }
+
+        match mode {
+            ParserMode::PRINT => {},
+            ParserMode::UPDATE => {},
+            ParserMode::MEMWRITE => {
+                buf.push(c)
+            },
+            ParserMode::COMMENT=> {
+                buf.push(c)
+            },
+            ParserMode::LABEL => {
+                buf.push(c)
+            },
+        };
+
+    }
+
+    for o in obj {
+        match o {
+            Object::Label(s) => {
+                println!("\\label{{{}}}" , s); 
+            },
+            Object::Code(s) => {
+                println!("\\code{{{}}}", s); 
+            },
+            Object::Comment(s) => {
+                println!("\\comment{{{}}}" , s); 
+            }
+        };
+    }
+
+    Ok(())
+}
